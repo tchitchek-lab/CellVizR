9ebe59d48bd91212904023671d50b551e2188d33
Gwendolyn-Marguerit <gmarguerit@gmail.com>
2022-11-30 18:21:10 +0100

    MAJ

--- R/export.R
+++ R/export.R
@@ -35,7 +35,7 @@ createFlowframe <- function(intensities) {
   intensities <- as.matrix(intensities)
   dataframe <- methods::as(data.frame(p), "AnnotatedDataFrame")
 
-  flowframe <- flowCore::flowFrame(intensities, dataframe, description = description)
+  flowframe <- suppressWarnings(flowCore::flowFrame(intensities, dataframe, description = description))
 
   return(flowframe)
 }
